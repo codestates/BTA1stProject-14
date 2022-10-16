604a24fe99b7b8722d4c60905fa126ccf1c91e1a
Woongsup Jung <woongsupjung@Woongsups-MacBook-Pro.local>
2022-10-16 14:48:14 +0900

    Load wallet balance using private_key

--- chrome_extension/js/load_wallet.js
+++ chrome_extension/js/load_wallet.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const sendLoadWalletRequest = async (url, private_key) =>
+{
+  const res = await axios.post(url, {private_key: private_key});
+  console.log(res.data.address);
+  console.log(res.data.balance);
+  return res;
+}
 function loadWallet() {
   console.log("Load Wallet!");
-  //TODO: Load Wallet
+  const private_key = document.getElementById('private_key').value;
+  const url = 'http://localhost:8000/load_wallet';
+  const res = sendLoadWalletRequest(url, private_key);
 }
-document.getElementById('loadWallet').addEventListener('click', loadWallet);
+document.getElementById('load_wallet').addEventListener('click', loadWallet);
+
