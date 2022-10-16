143cecd642a4dd013d17c0b2c43de6f1f552dd82
Woongsup Jung <woongsupjung@Woongsups-MacBook-Pro.local>
2022-10-16 16:33:52 +0900

    Combine all html files into one

--- chrome_extension/js/popup.js
+++ chrome_extension/js/popup.js
@@ -1,14 +1,29 @@
 'use strict';
 
+function changeClassDisplay(className, value){
+  var elements = document.getElementsByClassName(className);
+  for (var i=0; i<elements.length; i+=1){
+    elements[i].style.display = value;
+  }
+}
+
 function loadCreateWallet() {
-  chrome.windows.create({ url: chrome.runtime.getURL("html/create_wallet.html"),
-                          type: "popup", height : 500, width : 500 });
+  changeClassDisplay('main', 'none');
+  changeClassDisplay('create_wallet', 'block');
 }
 
 function loadLoadWallet() {
-  chrome.windows.create({ url: chrome.runtime.getURL("html/load_wallet.html"),
-                          type: "popup", height : 500, width : 500 });
+  changeClassDisplay('main', 'none');
+  changeClassDisplay('load_wallet', 'block');
+}
+
+function loadTransaction(){
+  changeClassDisplay('create_wallet', 'none');
+  changeClassDisplay('load_wallet', 'none');
+  changeClassDisplay('transaction', 'block');
 }
 
 document.getElementById('createWallet').addEventListener('click', loadCreateWallet);
 document.getElementById('loadWallet').addEventListener('click', loadLoadWallet);
+document.getElementById('load_wallet_btn').addEventListener('click', loadTransaction);
+document.getElementById('create_btn').addEventListener('click', loadTransaction);
